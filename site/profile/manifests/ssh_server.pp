28e4e7c8294f2f395b90fd0fb281016c7bd6267b
IkemanAV <78568761+IkemanAV@users.noreply.github.com>
2021-02-04 17:55:51 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDFcGiMGj5z4xA408CAtcX9rPAmZ+pIVHfUSAbpJt4M2tLw01X2S4GFQ9XuShp3qu2JiP0feAjROhMRuS4WbnLZ3Vk0EkWo0WQ+CmPDeYoAL3CBeDBQ9xv21f83UvvoiqSS6yUcNNXa8Xtsft2iXIXQrYD1WzBpn60vO5OkOKn2RBjj9Pxy+lQhKf8Blzweztfo5sDbehqhpKnBc6TmILGtyAQb4lmolzpAxcazGwQXThvdW6uIKcTTS4L+00J3Nue9gGr0oTQA56hcKnotVcsy4e5w8hMA2w4eWRn1NWP02poY0veMAEVEVDbW7M9IwxPA3XBM4l3FrLaAskCl6GRx',
 	}  
 }
