75089e8c8a9ba802ee7b99c7b43b0990e2e91812
IkemanAV <78568761+IkemanAV@users.noreply.github.com>
2021-02-04 19:10:22 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,19 +1,19 @@
 class minecraft {
-  file {'opt/minecraft':
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
   }
   package {'java':
     ensure => present,
   }
-  file {'opt/minecraft/eula.txt':
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true',
   }
-  file {'etc/systemd/system/minecraft.service':
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
